850ab534b91cf8246c68a473313ff218d10ad24f
mpaucar <MY_NAME@example.com>
2023-07-27 11:30:44 -0500

    test

--- serverTest.py
+++ serverTest.py
@@ -1,39 +1,62 @@
 from flask import Flask, request, jsonify
-from PIL import Image, ImageDraw, ImageFont
-import os
 
 app = Flask(__name__)
 
+# Datos de ejemplo: una lista para almacenar elementos
+data = [{"id": 1, "nombre": "Ejemplo 1"}, {"id": 2, "nombre": "Ejemplo 2"}]
 
-@app.route('/procesar_imagen', methods=['POST'])
-def procesar_imagen():
-    # Obtener la imagen desde la solicitud POST
-    imagen = request.files['imagen']
-    # Obtener la leyenda desde la solicitud POST (opcional)
-    leyenda = request.form.get('leyenda', '')
+# Ruta para obtener todos los elementos (GET)
 
-    # Procesar la imagen aquí (por ejemplo, aplicar filtros, reconocimiento de objetos, etc.)
-    # Guardar la imagen procesada
-    imagen_procesada_path = "./imagen_procesada.png"
-    imagen.save(imagen_procesada_path)
 
-    # Agregar la leyenda a la imagen procesada
-    if leyenda:
-        imagen_procesada = Image.open(imagen_procesada_path)
-        draw = ImageDraw.Draw(imagen_procesada)
-        width, height = imagen_procesada.size
-        # Calcula el tamaño de la fuente en base al alto de la imagen
-        font_size = int(height / 2)
-        font = ImageFont.truetype("arial.ttf", font_size)
-        text_width, text_height = draw.textsize(leyenda, font)
-        # Centra el texto en la imagen
-        text_position = ((width - text_width) // 2,
-                         (height - text_height) // 2)
-        draw.text(text_position, leyenda, fill=(255, 255, 255), font=font)
-        imagen_procesada.save(imagen_procesada_path)
+@app.route('/elementos', methods=['GET'])
+def obtener_elementos():
+    return jsonify(data)
 
-    # Devolver la ruta de la imagen procesada como respuesta
-    return jsonify({'imagen_procesada': imagen_procesada_path})
+# Ruta para obtener un elemento por su ID (GET)
+
+
+@app.route('/elementos/<int:id>', methods=['GET'])
+def obtener_elemento(id):
+    elemento = next((item for item in data if item["id"] == id), None)
+    if elemento:
+        return jsonify(elemento)
+    else:
+        return jsonify({"mensaje": "Elemento no encontrado"}), 404
+
+# Ruta para agregar un nuevo elemento (POST)
+
+
+@app.route('/elementos', methods=['POST'])
+def agregar_elemento():
+    nuevo_elemento = request.json
+    nuevo_elemento["id"] = len(data) + 1
+    data.append(nuevo_elemento)
+    return jsonify(nuevo_elemento), 201
+
+# Ruta para actualizar un elemento existente (PUT)
+
+
+@app.route('/elementos/<int:id>', methods=['PUT'])
+def actualizar_elemento(id):
+    elemento = next((item for item in data if item["id"] == id), None)
+    if elemento:
+        elemento_actualizado = request.json
+        elemento.update(elemento_actualizado)
+        return jsonify(elemento)
+    else:
+        return jsonify({"mensaje": "Elemento no encontrado"}), 404
+
+# Ruta para eliminar un elemento por su ID (DELETE)
+
+
+@app.route('/elementos/<int:id>', methods=['DELETE'])
+def eliminar_elemento(id):
+    elemento = next((item for item in data if item["id"] == id), None)
+    if elemento:
+        data.remove(elemento)
+        return jsonify({"mensaje": "Elemento eliminado correctamente"})
+    else:
+        return jsonify({"mensaje": "Elemento no encontrado"}), 404
 
 
 if __name__ == '__main__':
